30e67dbfeaa7cb986c98d862b0cc9ff8d3fd7e9e
Daniel <Daniel.halaas@gmail.com>
2026-02-21 15:17:57 +0100

    Update ContactPage.tsx

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -2,7 +2,7 @@ export function ContactPage() {
     return (
         <div className='flex flex-col h-full'>
             <div className='px-8 py-8'>
-                <h1 className='font-serif text-3xl text-foreground font-medium'>
+                <h1 className='text-2xl md:text-3xl font-serif font-semibold text-foreground tracking-tight'>
                     Kontakt Support
                 </h1>
                 <p className='text-sm text-muted-foreground mt-1'>
